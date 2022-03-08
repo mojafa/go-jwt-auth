df9e87ab1e5339214cd5a116a836d0b6ae428699
Jaafar Mohammed <jmohsa@gmail.com>
2022-03-08 23:54:32 +0300

    finished get user function in controllers

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,12 +1,17 @@
 package controllers
 
 import (
+	"context"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator"
 	"github.com/mojafa/go-jwt-auth/database"
+	"github.com/mojafa/go-jwt-auth/helpers"
+	"github.com/mojafa/go-jwt-auth/models"
 	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
@@ -25,10 +30,20 @@ func GetUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		userId := c.Param("userId")
 
-		if err := helper.MatchUSerTypeToUid(c, userId); err != nil {
+		if err := helpers.MatchUserTypeToUid(c, userId); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"userId": userId}).Decode(&user)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, user)
 	}
 }
 
